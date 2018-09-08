37e1114e06795ae5140651cb38b875cb72103481
Ajita Srivastava <ajitas4000@gmail.com>
2018-09-07 19:30:46 -0700

    adding files folders structure

---
+++ app/data/companion.js
@@ -0,0 +1,10 @@
+var peopleArray = [
+    {
+      name: "Jerome",
+      photo:"",
+      answers:[1,1,2,2,3,3,4,4,5,5]
+    }
+  ];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = peopleArray;
