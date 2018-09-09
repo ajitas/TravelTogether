79d242d4fe85bb4b5abb6d228d43c58d94de9068
Ajita Srivastava <ajitas4000@gmail.com>
2018-09-08 18:53:35 -0700

    adding basic logic

--- app/data/companion.js
+++ app/data/companion.js
@@ -1,9 +1,14 @@
 var peopleArray = [
     {
-      name: "Jerome",
-      photo:"",
+      name: "Buddy",
+      photoURL:"https://images.pexels.com/photos/356378/pexels-photo-356378.jpeg?auto=compress&cs=tinysrgb&h=350",
       answers:[1,1,2,2,3,3,4,4,5,5]
-    }
+    },
+    {
+        name: "Farley",
+        photoURL:"https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&h=350",
+        answers:[1,1,2,2,3,3,4,4,4,3]
+      }
   ];
   
   // Note how we export the array. This makes it accessible to other files using require.
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -29,12 +29,44 @@ module.exports = function(app) {
   // (ex. User fills out a reservation request... this data is then sent to the server...
   // Then the server saves the data to the tableData array)
   // ---------------------------------------------------------------------------
-
+  app.get("/api/match",function(req,res){
+      var minMatchFactor = Number.MAX_VALUE;
+      var matchIndex = -1;
+        for(var i =0;i<peopleData.length-1;i++){
+            matchFactor = 
+            Math.abs(peopleData[peopleData.length-1].answers[0] - peopleData[i].answers[0])+
+            Math.abs(peopleData[peopleData.length-1].answers[1] - peopleData[i].answers[1])+
+            Math.abs(peopleData[peopleData.length-1].answers[2] - peopleData[i].answers[2])+
+            Math.abs(peopleData[peopleData.length-1].answers[3] - peopleData[i].answers[3])+
+            Math.abs(peopleData[peopleData.length-1].answers[4] - peopleData[i].answers[4])+
+            Math.abs(peopleData[peopleData.length-1].answers[5] - peopleData[i].answers[5])+
+            Math.abs(peopleData[peopleData.length-1].answers[6] - peopleData[i].answers[6])+
+            Math.abs(peopleData[peopleData.length-1].answers[7] - peopleData[i].answers[7])+
+            Math.abs(peopleData[peopleData.length-1].answers[8] - peopleData[i].answers[8])+
+            Math.abs(peopleData[peopleData.length-1].answers[9] - peopleData[i].answers[9]);
+            
+            if(minMatchFactor >= matchFactor){
+                minMatchFactor = matchFactor;
+                matchIndex = i;
+            }
+        }
+        console.log(peopleData[matchIndex])
+        res.send(peopleData[matchIndex]);
+    });
   app.post("/api/companion", function(req, res) {
     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
     // It will do this by sending out the value "true" have a table
     // req.body is available since we're using the body-parser middleware
-    peopleData.push(req.body);
+    
+    var answersArray =[];
+    for(var i =0;i<req.body.answers.length;i++)
+        answersArray.push(parseInt(req.body.answers[i]));
+
+    var newCompanion = {name:req.body.name,
+                        photoURL:req.body.photoURL,
+                        answers:answersArray};
+
+    peopleData.push(newCompanion);
     res.json(true); 
   });
 
