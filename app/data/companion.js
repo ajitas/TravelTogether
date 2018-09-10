d47d844b0370db3c628639174b5ab77c0ab1c41f
Ajita Srivastava <ajitas4000@gmail.com>
2018-09-09 17:10:01 -0700

    adding styling

--- app/data/companion.js
+++ app/data/companion.js
@@ -1,12 +1,12 @@
 var peopleArray = [
     {
-      name: "Buddy",
-      photoURL:"https://images.pexels.com/photos/356378/pexels-photo-356378.jpeg?auto=compress&cs=tinysrgb&h=350",
+      name: "Leonardo DiCaprio",
+      photoURL:"http://specials-images.forbesimg.com/imageserve/558c0172e4b0425fd034f8ba/280x425.jpg?fit=scale&background=000000",
       answers:[1,1,2,2,3,3,4,4,5,5]
     },
     {
-        name: "Farley",
-        photoURL:"https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&h=350",
+        name: "Celine Dion",
+        photoURL:"https://s3.amazonaws.com/busites_www/celinedion/content/articles/16-0906.jpg",
         answers:[1,1,2,2,3,3,4,4,4,3]
       }
   ];
